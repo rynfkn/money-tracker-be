ef1ef4dbe6e25a9f1c47ad35d34f4327da109e6b
rynfkn <ryanfikri.re@gmail.com>
2025-12-24 22:38:20 +0700

    feat: add wallet route to the main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import healthRoute from "./health.route.js"
 import userRoute from "./user.route.js"
+import walletRoute from "./wallet.route.js"
 
 const router = Router();
 
 router.use("/health", healthRoute);
 
 router.use("/users", userRoute);
+
+router.use("/wallet", walletRoute);
 
 export default router;
