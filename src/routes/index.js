d9a8aaa0215e88cb6a1fb8e4fd07b48773735f35
rynfkn <ryanfikri.re@gmail.com>
2025-12-26 20:26:58 +0700

    feat: create tables for Categories, transactions, User, and wallets with foreign key constraints

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import healthRoute from "./health.route.js"
 import userRoute from "./user.route.js"
 import walletRoute from "./wallet.route.js"
 import categoriesRoute from "./categories.route.js"
+import transactionRoute from "./transaction.route.js"
 
 const router = Router();
 
@@ -13,5 +14,7 @@ router.use("/users", userRoute);
 router.use("/wallet", walletRoute);
 
 router.use("/categories", categoriesRoute);
+
+router.use("/transactions", transactionRoute);
 
 export default router;
