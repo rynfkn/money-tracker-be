c5bae83d6db518c2d2e2c0b1bc79cac2a57ae0f0
rynfkn <ryanfikri.re@gmail.com>
2026-01-05 14:58:19 +0700

    fix: add middlware and set credentials for cors

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
-// import { notFoundMiddleware } from "./middlewares/notfound.middleware.js";
-// import { errorMiddleware } from "./middlewares/error.middleware.js";
+import { notFoundMiddleware } from "./middlewares/notfound.middleware.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 import routes from "./routes/index.js";
 
@@ -15,7 +15,7 @@ app.use(
     cors({
         // origin: env.CORS_ORIGIN === "*" ? true : env.CORS_ORIGIN,
         origin: true,
-        credentials: false,
+        credentials: true,
     })
 );
 
@@ -27,18 +27,7 @@ app.use(express.urlencoded({
 // app.use(morgan(env.NODE_ENV == "production" ? "combined" : "dev"));
 app.use(morgan("combined"));
 
-app.get("/", (req, res) => {
-  res.status(200).send("OK - Express is running on Azure");
-});
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    ok: true,
-    uptime: process.uptime(),
-    timestamp: new Date().toISOString(),
-    node: process.version,
-  });
-});
 app.use("/api/v1", routes);
 
-// app.use(notFoundMiddleware);
-// app.use(errorMiddleware);
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
